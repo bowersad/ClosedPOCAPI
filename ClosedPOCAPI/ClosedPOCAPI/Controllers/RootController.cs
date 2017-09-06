75b097f9d9a42540991d847d564d9a6784f7ded7
bowersad <bowersad@gmail.com>
2017-09-06 08:35:33 -0400

    Initial Commit

---
+++ ClosedPOCAPI/ClosedPOCAPI/Controllers/RootController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClosedPOCAPI.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        public RootController()
+        {
+
+        }
+
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var result = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(result);
+        }
+    }
+}
